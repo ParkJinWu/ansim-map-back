2b5c61b4f52eb0040655df296ebdfa114969ad69
ParkJinWu <onu0311@gmail.com>
2026-01-23 23:17:28 +0900

    fc

---
+++ src/main/java/com/ansim/map/AnsimMapApplication.java
@@ -0,0 +1,13 @@
+package com.ansim.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsimMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsimMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ansim/map/AnsimMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansim.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsimMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
