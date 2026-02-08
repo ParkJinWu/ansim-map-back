93fdf085342d0d0ab4b1142aa336cb17a60ec1ce
ParkJinWu <onu0311@gmail.com>
2026-02-08 23:20:58 +0900

    fix : config cors 통합 및 수정

--- src/main/java/com/ansim/map/global/config/WebConfig.java
+++ src/main/java/com/ansim/map/global/config/WebConfig.java
@@ -3,24 +3,12 @@ package com.ansim.map.global.config;
 import com.ansim.map.global.common.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // 프론트엔드 URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver());
