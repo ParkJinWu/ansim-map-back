3d2792b86d11b3ffb919f2da59ebb06e6e32c53a
ParkJinWu <onu0311@gmail.com>
2026-02-10 23:01:56 +0900

    feat : ErrorCode 추가

--- src/main/java/com/ansim/map/global/exception/ErrorCode.java
+++ src/main/java/com/ansim/map/global/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "서버 내부 오류가 발생했습니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C002", "잘못된 입력값입니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C003", "허용되지 않은 메소드입니다."),
+    ACCESS_DENIED(HttpStatus.BAD_REQUEST,"C004","해당 요청에 대한 권한이 없습니다."),
 
     // 인증 (AUTH)
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A001", "인증이 필요합니다."),
@@ -21,7 +22,11 @@ public enum ErrorCode {
 
     // 회원 (MEMBER)
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "M001", "존재하지 않는 회원입니다."),
-    EMAIL_DUPLICATION(HttpStatus.BAD_REQUEST, "M002", "이미 존재하는 이메일입니다.");
+    EMAIL_DUPLICATION(HttpStatus.BAD_REQUEST, "M002", "이미 존재하는 이메일입니다."),
+
+    // 즐겨찾기 (FAVORITE)
+    FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND,"F001","존재하지 않는 즐겨찾기입니다."),
+    FAVORITE_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST,"FOO2" ,"즐겨찾기는 최대 5개까지만 등록 가능합니다.");
 
     private final HttpStatus status;
     private final String code;
